64e0e7d93cb973d92ca9021fd7d004049eda1281
William Rockwood <wrockwood@tadl.org>
2026-05-24 18:34:35 -0400

    Simplify users directory view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -50,6 +50,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "td", text: "viewer@example.com"
+    assert_select "td", text: /Existing local note/
+    assert_select "textarea[name='plex_user_note[notes]']", count: 0
+    assert_select "button", text: "Apply to selected", count: 0
+    assert_select "input[name='user_ids[]']", count: 0
     assert_select "p", text: "1 user shown"
   end
 
